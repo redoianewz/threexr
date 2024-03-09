56e65fb28bda020d0d2b2acb6905a21f9a4e3cf2
redoianewz <104329601+redoianewz@users.noreply.github.com>
2024-03-09 22:51:01 +0100

    rers

--- cube/xr-image.js
+++ cube/xr-image.js
@@ -112,7 +112,7 @@ document.body.appendChild(
   ARButton.createButton(renderer, {
     requiredFeatures: ["hit-test"],
     optionalFeatures: ["dom-overlay", "dom-overlay-for-handheld-ar"],
-    domOverlay: { root: document.body },
+    domOverlay: { root: document.getElementById("stor") },
   })
 );
 
